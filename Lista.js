5eb278972855a5f7b7a1aa66deb5b0a406df9b10
vitoriagabriela2512 <165718317+vitoriagabriela2512@users.noreply.github.com>
2025-05-03 16:42:58 -0300

    Add files via upload

---
+++ Lista.js
@@ -0,0 +1,30 @@
+const form = document.getElementById("form-lista");
+const lista = document.getElementById("lista-itens");
+const btnLimpar = document.getElementById("limpar-lista");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const produto = document.getElementById('produto').value.trim();
+  const quantidade = document.getElementById('quantidade').value.trim();
+
+  if (produto === '' || quantidade === '' || quantidade <= 0) {
+    alert('Preencha o nome do produto e uma quantidade válida!');
+    return;
+  }
+
+  const item = document.createElement('li');
+  item.textContent = `${produto} (${quantidade})`;
+
+  item.addEventListener('click', function () {
+    item.classList.toggle('riscado');
+  });
+
+  lista.appendChild(item);
+
+  form.reset();
+});
+
+btnLimpar.addEventListener('click', function () {
+  lista.innerHTML = '';
+});
